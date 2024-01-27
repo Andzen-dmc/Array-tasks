9775184473512d6ad66d9bac185a4894c2abf5a5
Andzen-dmc <156898310+Andzen-dmc@users.noreply.github.com>
2024-01-27 18:23:10 +0300

    Add files via upload

---
+++ Задачи по массивам 2/Задачи по массивам 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создание массива строк
+        string[] stringArray = { "Mango", "lemon", "orange", "pomegranate", "watermelon  " };
+
+        Console.WriteLine("Введите строку для поиска:");
+
+        // Ввод строки от пользователя
+        string userInput = Console.ReadLine();
+
+        // Проверка наличия строки в массиве
+        bool isStringFound = false;
+
+        foreach (string str in stringArray)
+        {
+            if (str.Equals(userInput, StringComparison.OrdinalIgnoreCase))
+            {
+                isStringFound = true;
+                break;
+            }
+        }
+
+        // Вывод результата
+        if (isStringFound)
+        {
+            Console.WriteLine("Строка найдена!");
+        }
+        else
+        {
+            Console.WriteLine("Строка не найдена!");
+        }
+    }
+}
